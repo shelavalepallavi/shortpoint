5a6744899e3c619abfe5f73020c2ddc17a29526f
shelavale3795 <pallavishelavale3795@gmail.com>
2025-02-22 22:16:18 +0530

    make responsive

--- app/page.js
+++ app/page.js
@@ -12,10 +12,10 @@ const poppins = localFont({
 const page = () => {
   return (
     <main className='bg-purple-100 '>
-      <section className='grid md:grid-cols-2 [grid-template-columns:repeat(2,1fr)_reversed] h-[92vh] md:h-[60vh] pt-24'>
+      <section className='grid md:grid-cols-2 [grid-template-columns:repeat(2,1fr)_reversed] h-[92vh] md:h-[60vh] pt-4 md:pt-24'>
         <div className='flex flex-col gap-4 items-center justify-center'>
-          <p className={` font-bold text-2xl ${poppins.className}` }>The best URL shortner in the Market</p>
-          <p className='px-28 text-center'>We are the most straightforward URL shortener in the world. Most of the url shorteners will track you or ask youu to give your details for login. We understand your needs and hence we have created this URL shortener</p>
+          <p className={` font-bold text-lg sm:text-2xl ${poppins.className}` }>The best URL shortner in the Market</p>
+          <p className='px-28 text-center hidden sm:block'>We are the most straightforward URL shortener in the world. Most of the url shorteners will track you or ask youu to give your details for login. We understand your needs and hence we have created this URL shortener</p>
           <div className='flex gap-3 text-white'>
           <Link href="/shorten" className=''><button className=' bg-purple-500 p-1 rounded-lg shadow-lg px-3 font-bold'>Try Now</button></Link>
           <Link href="/github" target='_blank' ><button className='bg-purple-500 p-1 rounded-lg shadow-lg px-3 font-bold'>Github</button></Link>
--- components/Navbar.js
+++ components/Navbar.js
@@ -4,18 +4,20 @@ import Link from 'next/link'
 
 const Navbar = () => {
   return (
-    <nav className='h-24 sm:h-16 bg-purple-700  text-white  px-3   sm:flex sm:justify-between sm:items-center text-center'>
+    <nav className='h-32 sm:h-24 md:h-16 bg-purple-700  text-white  px-3 flex flex-col  md:flex-row justify-center md:justify-between items-center  md:items-center text-center'>
       <div className='logo font-bold text-2xl pb-2'>
       <Link href="/">ShortPoint</Link>
       </div>
-      <ul className='flex gap-4 items-center px-12 sm:ps-0'>
-        <Link href="/"><li>Home</li></Link>
-        <Link href="/about"><li>About</li></Link>
-        <Link href="/shorten"><li>Shorten</li></Link>
-        <Link href="/contact"><li>Contact us</li></Link>
+      <ul className='flex flex-col  sm:flex-row  gap-2 sm:gap-4 items-center px-12 sm:ps-0'>
+        <div className='flex gap-4 '>
+        <Link href="/" className='text-sm sm:text-base'><li>Home</li></Link>
+        <Link href="/about" className='text-sm sm:text-base'><li>About</li></Link>
+        <Link href="/shorten" className='text-sm sm:text-base'><li>Shorten</li></Link>
+        <Link href="/contact" className='text-sm sm:text-base'><li>Contact us</li></Link>
+        </div>
         <li className='flex gap-3'>
-          <Link href="/shorten" ><button className='bg-purple-500 p-1 rounded-lg shadow-lg px-3 font-bold'>Try Now</button></Link>
-          <Link href="/github" target='_blank'><button className='bg-purple-500 p-1 rounded-lg shadow-lg px-3 font-bold'>Github</button></Link>
+          <Link href="/shorten" ><button className='bg-purple-500 p-1 rounded-lg shadow-lg px-3 font-bold text-sm sm:text-base'>Try Now</button></Link>
+          <Link href="/github" target='_blank'><button className='bg-purple-500 p-1 text-sm sm:text-base rounded-lg shadow-lg px-3 font-bold'>Github</button></Link>
         </li>
       </ul>
     </nav>
